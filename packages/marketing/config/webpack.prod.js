880744377c6e79f9a796f960e3e1692bb53360c9
Peter Labib <peter.adel.labib@gmail.com>
2023-03-11 23:30:29 +0200

    added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PROCDUCTION_DOMAIN
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
