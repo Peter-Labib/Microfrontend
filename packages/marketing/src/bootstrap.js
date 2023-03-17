2c1eb1930f3b2291fa868cfb707390e0c5c3d6e6
Peter Labib <peter.adel.labib@gmail.com>
2023-03-18 00:09:34 +0200

    container to child communication

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -6,10 +6,15 @@ import App from './App';
 // Mount function to start up the app
 const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
-  
+
   if (onNavigate) history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      if (pathname !== nextPathname) history.push(nextPathname);
+    },
+  };
 };
 
 // If we are in development and in isolation,
